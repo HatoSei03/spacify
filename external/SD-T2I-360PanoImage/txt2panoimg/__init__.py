e5392c5f9a2136778bbeb2e184433ab6476141bc
Lê Ngọc Thảo <lengocthao.dev@gmail.com>
2025-07-22 15:28:02 +0700

    update
    try to add 3dgs repo

--- external/SD-T2I-360PanoImage/txt2panoimg/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pipeline_base import StableDiffusionBlendExtendPipeline
-from .pipeline_sr import StableDiffusionControlNetImg2ImgPanoPipeline
-from .text_to_360panorama_image_pipeline import Text2360PanoramaImagePipeline
